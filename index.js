a97478e77a828e058516daa3908e0f48f469e084
The Jared Wilcurt <TheJaredWilcurt@users.noreply.github.com>
2022-01-16 14:03:40 -0500

    Update max-attributes-per-line
    the API changed again

--- index.js
+++ index.js
@@ -98,10 +98,11 @@ module.exports = {
     'vue/max-attributes-per-line': [
       'error',
       {
-        'singleline': 3,
+        'singleline': {
+          'max': 3
+        },
         'multiline': {
-          'max': 1,
-          'allowFirstLine': false
+          'max': 1
         }
       }
     ],
